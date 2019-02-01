28b4f46299d65a10c41f5b4fc574f962b1c7d168
isac1208 <44215826+isac1208@users.noreply.github.com>
2019-01-31 16:45:54 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$(".button1").click(function(){
+ $(".magic1").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$(".button2").click(function(){
+  $(".magic2").toggle();
+});
+
+
+
+//Spell #3
+
+$(".button3").click(function(){
+  $(".magic3").slideToggle();
+});
+
+
+
